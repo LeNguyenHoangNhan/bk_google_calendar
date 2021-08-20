89eee2fa8f88c4f97e227cc66823cc7ef5d849ac
LeNguyenHoangNhan <public.hoangnhanlenguyen@gmail.com>
2021-08-20 22:33:34 +0700

    Remove sensitive content

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Footer.css';
 function Footer() {
     return <div className="footer_wrapper">
-        <p>Developed by <a className='link no-underline' target="_blank" href="https://github.com/LeNguyenHoangNhan" rel="noopener noreferrer">LeNguyenHoangNhan</a> with ❤️ for Thanh Tâm</p>
+        <p>Developed by <a className='link no-underline' target="_blank" href="https://github.com/LeNguyenHoangNhan" rel="noopener noreferrer">LeNguyenHoangNhan</a> with ❤</p>
     </div>
 }
 
