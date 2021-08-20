49c1deb36f92e87059f8eee4d9125bc70817f11e
LeNguyenHoangNhan <public.hoangnhanlenguyen@gmail.com>
2021-08-20 14:43:23 +0700

    Add footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Footer.css';
+function Footer() {
+    return <div className="footer_wrapper">
+        <p>Developed by <a className='link no-underline' target="_blank" href="https://github.com/LeNguyenHoangNhan" rel="noopener noreferrer">LeNguyenHoangNhan</a> with ❤️ for Thanh Tâm</p>
+    </div>
+}
+
+export default Footer;
